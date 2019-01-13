37037bf265c2ef2b22396abe24b800ebd5b5134b
Freda Lai <fredalai00@gmail.com>
2019-01-13 17:09:14 +0800

    Update source code for add customize config of rule 1

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const constants = require('./constants');
 const url = require('url');
 
-module.exports = function (data) {
-  const { method, url: dataUrl = '' } = data;
-  const urlObj = url.parse(dataUrl);
-  if (method !== constants.GET) {
+module.exports = function (config) {
+  return function (data) {
+    const { method, url: dataUrl = '' } = data;
+    const urlObj = url.parse(dataUrl);
+    if (method !== constants.GET) {
+      return data;
+    }
+
+    if (urlObj.pathname === '/domain/users') {
+      urlObj.pathname = '/api/domain/users';
+      data.url = url.format(urlObj);
+    }
+
     return data;
-  }
-
-  if (urlObj.pathname === '/domain/users') {
-    urlObj.pathname = '/api/domain/users';
-    data.url = url.format(urlObj);
-  }
-
-  return data;
+  };
 }
