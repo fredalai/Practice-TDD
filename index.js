4b1744e964570ae5b0a24e5ff0b75e3370b8baf0
Freda Lai <fredalai00@gmail.com>
2019-01-13 16:44:19 +0800

    Add source code for scenario 3

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const constants = require('./constants');
 const url = require('url');
 
 module.exports = function (data) {
-  const { url: dataUrl = '' } = data;
+  const { method, url: dataUrl = '' } = data;
   const urlObj = url.parse(dataUrl);
+  if (method !== constants.GET) {
+    return data;
+  }
+
   if (urlObj.pathname === '/domain/users') {
     urlObj.pathname = '/api/domain/users';
     data.url = url.format(urlObj);
