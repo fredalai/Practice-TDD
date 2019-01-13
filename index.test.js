0f6aee0e43ea101ec7d9aea05c4b7b8a2b252056
Freda Lai <fredalai00@gmail.com>
2019-01-13 16:23:41 +0800

    Update test for scenario 1

--- index.test.js
+++ index.test.js
@@ -19,19 +19,30 @@ const getObjDataByConfig = (config = {}) => {
 };
 
 describe('Rule 1', () => {
-  it('Scenario 1 - input and output are object type', () => {
-    const obj = getObjDataByConfig();
-    const result = rule(obj);
+  describe('Scenario 1 - Check the function', () => {
+    it('Scenario 1 - input and output are object type', () => {
+      const obj = getObjDataByConfig();
+      const result = rule(obj);
 
-    expect(result).toBe(obj);
+      expect(result).toEqual({
+        url: 'http://www.aroundyourlife.com/test',
+        method: GET,
+        headers: {
+          Cookie: 'name=value; name2=value2',
+          'Content-Type': 'application/json',
+        }
+      });
+    });
   });
 
-  it('Scenario 2 - Update URL path name /api/domain/users instead of /domain/users', () => {
+  describe('Scenario 2 - Execute main task when method is GET', () => {
     const obj = getObjDataByConfig({
       url: 'http://www.aroundyourlife.com/domain/users',
     });
     const result = rule(obj);
 
-    expect(result.url).toBe('http://www.aroundyourlife.com/api/domain/users');
+    it('Update URL path name /api/domain/users instead of /domain/users', () => {
+      expect(result.url).toBe('http://www.aroundyourlife.com/api/domain/users');
+    });
   });
 });
