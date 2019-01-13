6b25efa1b771fe0a69193a5920631014b3fb675d
Freda Lai <fredalai00@gmail.com>
2019-01-13 16:12:38 +0800

    Add source code for scenario 2

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const url = require('url');
+
 module.exports = function (data) {
+  const { url: dataUrl = '' } = data;
+  const urlObj = url.parse(dataUrl);
+  if (urlObj.pathname === '/domain/users') {
+    urlObj.pathname = '/api/domain/users';
+    data.url = url.format(urlObj);
+  }
+
   return data;
 }
